e900fefbeb8699720c556cd061e2cadb01b9e433
gisly <gisly@list.ru>
2018-07-08 01:13:01 +0300

    added logging, argument parsing

--- src/train_ocr.py
+++ src/train_ocr.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # coding=utf-8
 __author__ = 'gisly'
 
@@ -6,9 +7,12 @@ import os
 import shutil
 import time
 import argparse
+import logging
 
-TESSERACT_FOLDER = '/home/gisly/evenki_tesseract'
-TESSDATA_FOLDER = '/home/gisly/tesseract/tesseract/'
+
+#TESSERACT_FOLDER = '/home/gisly/evenki_tesseract'
+#TESSDATA_FOLDER = '/home/gisly/tesseract-ocr/tessdata/tesseract/'
+
 TESSERACT_FILENAMES = ['unicharset', 'inttemp', 'normproto', 'pffmtable']
 
 COMMAND_CREATE_IMAGE = "text2image --text=\"%(FILENAME)s\" " \
@@ -16,14 +20,14 @@ COMMAND_CREATE_IMAGE = "text2image --text=\"%(FILENAME)s\" " \
                        "--font=%(FONT_NAME)s --fonts_dir=\"%(FONT_DIR)s\""
 
 COMMAND_CREATE_BOX = "tesseract \"%(FOLDER)s/%(LANGUAGE)s.%(FONT_NAME)s.exp%(INDEX)s\".tif " \
-                     "\"%(FOLDER)s/%(LANGUAGE)s.%(FONT_NAME)s.exp%(INDEX)s\" %(TESSDATA_FOLDER)s/tessdata/configs/box.train"
+                     "\"%(FOLDER)s/%(LANGUAGE)s.%(FONT_NAME)s.exp%(INDEX)s\" %(TESSDATA_FOLDER)s/configs/box.train"
 
 COMMAND_EXTRACT_UNICHAR = "unicharset_extractor %(BOX_FILES)s"
 COMMAND_UNICHAR_PROPS = "set_unicharset_properties -U  \"%(TESSERACT_FOLDER)s/unicharset\" " \
                         "-O \"%(FOLDER)s/output_unicharset\" --script_dir=training/langdata"
 
 COMMAND_MFTRAINING = "mftraining -F %(TESSERACT_FOLDER)s/font_properties -U \"%(FOLDER)s/output_unicharset\" " \
-                     "-O \"%(FOLDER)s/%(LANGUAGE)s.unicharset\" %(TR_FILES)s"
+                     "-O \"%(TESSERACT_FOLDER)s/%(LANGUAGE)s.unicharset\" %(TR_FILES)s"
 
 COMMAND_CNTRAINING = "cntraining %(TR_FILES)s"
 
@@ -32,19 +36,23 @@ COMMAND_COMBINE_TESSDATA = "combine_tessdata \"%(FOLDER)s/%(LANGUAGE)s.\""
 EXTENSION_TEXT = '.txt'
 
 
-def process_training_data(training_folder, output_folder, language, font_name, font_dir, is_verbose):
+def process_training_data(training_folder, output_folder, language, font_name, font_dir, tessdata_folder,
+                          is_verbose=False):
     create_necessary_folders(output_folder, is_verbose)
-    delete_existing_tesseract_files(training_folder, language)
-    box_files, tr_files = process_training_files(training_folder, output_folder, language, font_name, font_dir)
-    time.sleep(10)
+    #delete_existing_tesseract_files(training_folder, language, is_verbose)
+    box_files, tr_files = process_training_files(training_folder, output_folder,
+                                                 language, font_name, font_dir, tessdata_folder, is_verbose)
+    time.sleep(2)
 
-    process_box_files(box_files, tr_files, output_folder, language, font_name, font_dir)
+    process_box_files(training_folder, box_files, tr_files, output_folder, language, font_name, font_dir, tessdata_folder, is_verbose)
     time.sleep(20)
 
-    collect_training_data(output_folder, language)
+    collect_training_data(training_folder, output_folder, language, is_verbose)
 
 
-def process_training_files(training_folder, output_folder, language, font_name, font_dir):
+def process_training_files(training_folder, output_folder, language,
+                           font_name, font_dir, tessdata_folder,
+                           is_verbose=False):
     example_index = 0
     box_files = []
     tr_files = []
@@ -52,57 +60,73 @@ def process_training_files(training_folder, output_folder, language, font_name, 
         if is_text_file(filename):
             example_index += 1
             full_filename = os.path.join(training_folder, filename)
-            box_file, tr_file = process_training_text(full_filename, output_folder,
+            if is_verbose:
+                logging.info("Starting to process %s (%s)" % (full_filename, example_index))
+            box_file, tr_file = process_training_text(training_folder,
+                                                      full_filename, output_folder,
                                                       language, font_name,
-                                                      example_index, font_dir)
+                                                      example_index, font_dir, tessdata_folder,
+                                                      is_verbose)
             box_files.append(box_file)
             tr_files.append(tr_file)
 
-            print('=====')
-            print(filename)
-            if example_index > 1000:
-                break
+            if example_index % 100 == 0:
+                logging.info("Processed training file %s" % example_index)
 
     return box_files, tr_files
 
 
-def process_training_text(filename, output_folder, language, font_name, example_index, font_dir):
+def process_training_text(training_folder, filename, output_folder, language,
+                          font_name, example_index, font_dir,
+                          tessdata_folder, is_verbose):
     command_list = [COMMAND_CREATE_IMAGE,
                     COMMAND_CREATE_BOX
                     ]
 
-    res = process_tesseract_command_str(command_list, filename, output_folder, language, font_name, example_index,
-                                        font_dir)
-    print(res)
+    res = process_tesseract_command_str(training_folder, command_list, filename, output_folder, language,
+                                        font_name, example_index,
+                                        font_dir, tessdata_folder, is_verbose=is_verbose)
+    if is_verbose:
+        logging.info("process_training_text:%s" % res)
 
     resulting_filename = replace_properties("%(FOLDER)s/%(LANGUAGE)s.%(FONT_NAME)s.exp%(INDEX)s",
-                                            filename, output_folder,
-                                            language, font_name, example_index, font_dir)
+                                            filename, training_folder,
+                                            output_folder,
+                                            language, font_name, example_index,
+                                            font_dir, tessdata_folder)
 
     box_file = resulting_filename + '.box'
     tr_file = resulting_filename + '.tr'
     return box_file, tr_file
 
 
-def collect_training_data(output_folder, language):
-    prepare_files(output_folder, language)
+def collect_training_data(training_folder, output_folder, language, is_verbose=False):
+    prepare_files(training_folder, output_folder, language)
     command_list = [COMMAND_COMBINE_TESSDATA]
 
-    res = process_tesseract_command_str(command_list, '', output_folder, language, '', 0, '')
-    print(res)
+    res = process_tesseract_command_str(training_folder,
+                                        command_list, '', output_folder, language, '', 0, '',
+                                        is_verbose=is_verbose)
+    if is_verbose:
+        logging.info("collect_training_data:%s", res)
 
 
-def prepare_files(output_folder, language):
-    copy_tesseract_files(output_folder, language)
+def prepare_files(training_folder, output_folder, language):
+    copy_tesseract_files(training_folder, output_folder, language)
 
 
-def copy_tesseract_files(output_folder, language):
+def copy_tesseract_files(training_folder, output_folder, language):
+    output_language_folder = create_necessary_folders(os.path.join(output_folder, language))
     for filename in TESSERACT_FILENAMES:
-        copy_file_from_folder_to_folder(filename, TESSERACT_FOLDER, output_folder, language + '.')
+        if filename == 'unicharset':
+            filename = language + '.' + filename
+        copy_file_from_folder_to_folder(filename, training_folder, output_language_folder, language + '.')
 
 
 # TODO: MFTRAINING???
-def process_box_files(box_files, tr_files, output_folder, language, font_name, font_dir):
+def process_box_files(training_folder,
+                      box_files, tr_files, output_folder, language, font_name, font_dir, tessdata_folder,
+                      is_verbose=False):
     box_filenames = ' '.join(['"' + box_file + '"' for box_file in box_files])
     tr_filenames = ' '.join(['"' + tr_file + '"' for tr_file in tr_files])
 
@@ -111,32 +135,45 @@ def process_box_files(box_files, tr_files, output_folder, language, font_name, f
                     COMMAND_MFTRAINING,
                     COMMAND_CNTRAINING]
 
-    res = ''
+    res = 'no result'
     try:
-        res = process_tesseract_command_str(command_list, '', output_folder, language, font_name, 0,
-                                            font_dir, box_filenames, tr_filenames)
+        res = process_tesseract_command_str(training_folder,
+                                            command_list, '', output_folder, language, font_name, 0,
+                                            font_dir, tessdata_folder,
+                                            box_filenames, tr_filenames,
+                                            is_verbose=is_verbose)
     except Exception as e:
-        print("Error:" + str(e))
+        logging.error("Error processing process_box_files:" + str(e))
 
-    print(res)
+    if is_verbose:
+        logging.info("process_box_files: %s" % res)
 
 
-def process_tesseract_command_str(command_list, filename, output_folder,
+def process_tesseract_command_str(training_folder, command_list, filename, output_folder,
                                   language, font_name, example_index, font_dir,
-                                  box_files=None, tr_files=None):
-    move_to_dir = "cd " + TESSERACT_FOLDER
+                                  tessdata_folder,
+                                  box_files=None, tr_files=None,
+                                  is_verbose=False):
+    move_to_dir = "cd " + output_folder
     command_list_tesseract = [move_to_dir] + command_list
-    command_str = [replace_properties(command, filename, output_folder,
-                                      language, font_name, example_index, font_dir, box_files, tr_files)
+    command_str = [replace_properties(command, filename, training_folder,
+                                      output_folder,
+                                      language, font_name, example_index, font_dir,
+                                      tessdata_folder,
+                                      box_files, tr_files)
                    for command in command_list_tesseract]
     for command_str_element in command_str:
-        print(command_str_element)
-        res = subprocess.check_output(move_to_dir + '&' + command_str_element, shell=True).decode('cp1251')
+        if is_verbose:
+            logging.info("process_tesseract_command_str: %s" % command_str_element)
+        res = subprocess.check_output(move_to_dir + '&' + command_str_element, shell=True,
+                                      stderr=subprocess.STDOUT).decode('utf-8')
     return res
 
 
-def replace_properties(command, filename, output_folder, language,
+def replace_properties(command, filename,
+                       training_folder, output_folder, language,
                        font_name, example_index, font_dir,
+                       tessdata_folder,
                        box_files=None, tr_files=None):
     return command % {'FILENAME': filename,
                       'FOLDER': output_folder,
@@ -144,17 +181,17 @@ def replace_properties(command, filename, output_folder, language,
                       'FONT_NAME': font_name,
                       'INDEX': example_index,
                       'FONT_DIR': font_dir,
-                      'TESSERACT_FOLDER': TESSERACT_FOLDER,
+                      'TESSERACT_FOLDER': training_folder,
                       'BOX_FILES': box_files,
                       'TR_FILES': tr_files,
-                      'TESSDATA_FOLDER': TESSDATA_FOLDER}
+                      'TESSDATA_FOLDER': tessdata_folder}
 
 
 def is_text_file(filename):
     return filename.endswith(EXTENSION_TEXT)
 
 
-def delete_existing_tesseract_files(folder, language_name=None):
+def delete_existing_tesseract_files(folder, language_name=None, is_verbose=False):
     if language_name:
         prefix = language_name + '.'
     else:
@@ -163,22 +200,23 @@ def delete_existing_tesseract_files(folder, language_name=None):
         full_filename = os.path.join(folder, language_name + filename)
         if os.path.exists(full_filename):
             os.remove(full_filename)
+            if is_verbose:
+                logging.info("Removing folder %s " % full_filename)
 
     for filename in os.listdir(folder):
-        if filename.startswith(prefix) and (filename.endswith('.box')
-                                            or filename.endswith('.tr')
-                                            or filename.endswith('.tif')):
+        if filename.startswith(prefix) and is_generated_file(filename):
             full_filename = os.path.join(folder, filename)
             os.remove(full_filename)
+            logging.info("Removing folder %s " % full_filename)
 
-def check_folder(folder_name):
+def check_file_exists(filename):
     """
-    checking if a folder with a given name exists
-    :param folder_name:
+    checking if a file with a given name exists
+    :param filename:
     :return:
     """
-    if not os.path.exists(folder_name):
-        raise ("Training folder %s does not exist" % folder_name)
+    if not os.path.exists(filename):
+        raise Exception("File %s does not exist" % filename)
 
 def create_necessary_folders(folder_name, is_verbose=False):
     """
@@ -190,7 +228,8 @@ def create_necessary_folders(folder_name, is_verbose=False):
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
         if is_verbose:
-            print("created output folder: %s" % folder_name)
+            logging.info("created output folder: %s" % folder_name)
+    return folder_name
 
 def copy_file_from_folder_to_folder(filename, from_folder, to_folder, prefix='', is_verbose=False):
     """
@@ -204,20 +243,27 @@ def copy_file_from_folder_to_folder(filename, from_folder, to_folder, prefix='',
     """
     shutil.copy(os.path.join(from_folder, filename), os.path.join(to_folder, prefix + filename))
     if is_verbose:
-        print('copied:%s to %s' % (os.path.join(from_folder, filename), os.path.hoin(to_folder, prefix + filename)))
+        logging.info('copied:%s to %s' % (os.path.join(from_folder, filename), os.path.hoin(to_folder, prefix + filename)))
+
+def is_generated_file(filename):
+    return filename.endswith('.box') or filename.endswith('.tr') or filename.endswith('.tif')
 
 def main():
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
     parser = argparse.ArgumentParser(description='run OCR training process')
-    parser.add_argument("-i", "--input",
+    parser.add_argument("input",
                         help="training folder")
-    parser.add_argument("-o", "--output",
+    parser.add_argument("output",
                         help="result folder")
-    parser.add_argument("-l", "--language",
+    parser.add_argument("language",
                         help="language")
-    parser.add_argument("-fn", "--font_name",
+    parser.add_argument("font_name",
                         help="font name")
-    parser.add_argument("-ff", "--font_folder",
+    parser.add_argument("font_folder",
                         help="font folder")
+    parser.add_argument("tessdata_folder",
+                        help="tessdata folder")
     parser.add_argument("-v", "--verbose", action="store_true",
                         help="increase output verbosity")
 
@@ -228,11 +274,16 @@ def main():
     language = args.language
     font_name = args.font_name
     font_folder = args.font_folder
+    tessdata_folder = args.tessdata_folder
     is_verbose = args.verbose
 
-    check_folder(input_folder)
+    check_file_exists(input_folder)
+    check_file_exists(tessdata_folder)
+    check_file_exists(os.path.join(input_folder, "font_properties"))
 
-    process_training_data(input_folder, output_folder, language, font_name, font_folder, is_verbose)
+    process_training_data(input_folder, output_folder, language, font_name, font_folder, tessdata_folder,
+                          is_verbose)
+
 
 if __name__ == "__main__":
     main()
